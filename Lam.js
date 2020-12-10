54469684200c8a0729feb7fc90d488ee5a2f8773
Asad Ahmad <71641766+asadahmad123@users.noreply.github.com>
2020-12-10 12:48:36 +0530

    Update Lam.js

--- Lam.js
+++ Lam.js
@@ -1,29 +1,10 @@
-    LambdaTest selenium automation sample example
-    Configuration
-    ----------
-    username: Username can be found at automation dashboard
-    accessKey:  AccessKey can be generated from automation dashboard or profile section
- 
-    Result
-    -------
-    Execute NodeJS Automation Tests on LambdaTest Distributed Selenium Grid
-*/
 const webdriver = require('selenium-webdriver');
  
-/*
-    Setup remote driver
-    Params
-    ----------
-    platform : Supported platform - (Windows 10, Windows 8.1, Windows 8, Windows 7,  macOS High Sierra, macOS Sierra, OS X El Capitan, OS X Yosemite, OS X Mavericks)
-    browserName : Supported platform - (chrome, firefox, Internet Explorer, MicrosoftEdge, Safari)
-    version :  Supported list of version can be found at https://www.lambdatest.com/capabilities-generator/
-*/
- 
 // username: Username can be found at automation dashboard
-const USERNAME = '{username}';
+const USERNAME = 'creator.asad';
  
-// AccessKey:  AccessKey can be generated from automation dashboard or profile section
-const KEY = '{accessKey}';
+// Accesskey:  Accesskey can be generated from automation dashboard or profile section
+const KEY = 'R1MwvaZHT9qOGTP74hdUu5I5oisRu1ZJUVrPSBzhFJwrntTvCS';
  
 // gridUrl: gridUrl can be found at automation dashboard
 const GRID_HOST = 'hub.lambdatest.com/wd/hub';
@@ -31,10 +12,10 @@ const GRID_HOST = 'hub.lambdatest.com/wd/hub';
 function searchTextOnGoogle() {
     // Setup Input capabilities
     const capabilities = {
-        platform: 'windows 10',
-        browserName: 'chrome',
-        version: '67.0',
-        resolution: '1280x800',
+        platform: 'Windows 10',
+        browserName: 'Chrome',
+        version: '87.0',
+        resolution: '1024x768',
         network: true,
         visual: true,
         console: true,
@@ -54,7 +35,8 @@ function searchTextOnGoogle() {
  
     // navigate to a url, search for a text and get title of page
     driver.get('https://www.google.com/ncr').then(function() {
-        driver.findElement(webdriver.By.name('q')).sendKeys('LambdaTest\n').then(function() {
+        driver.findElement(webdriver.By.name('q')).sendKeys('LambdaTest
+').then(function() {
             driver.getTitle().then(function(title) {
                 setTimeout(function() {
                     console.log(title);
@@ -64,4 +46,5 @@ function searchTextOnGoogle() {
         });
     });
 }
+
 searchTextOnGoogle();
